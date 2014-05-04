1ce4f86b7d6fbca6e619d2529dde99e12c5e7754
Abundnce10 <michaelstitt@gmail.com>
2014-05-04 10:25:23 -0700

    Allow CORS for pinterest/fb endpoints

--- hello.py
+++ hello.py
@@ -2,12 +2,55 @@ import requests
 from socialanalytics import pinterest, facebook
 import json
 import pytz
-import datetime
-from flask import Flask, url_for, request, jsonify, render_template
+from datetime import timedelta, datetime
+from flask import Flask, url_for, request, jsonify, render_template, make_response, current_app 
 from werkzeug import SharedDataMiddleware
+from functools import update_wrapper
 
 
 app = Flask(__name__)
+
+
+### Cross Domains ###
+def crossdomain(origin=None, methods=None, headers=None, max_age=21600, attach_to_all=True, automatic_options=True):  
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
 
 
 #### Routes ####
@@ -23,6 +66,7 @@ def api_root():
 
 
 @app.route('/api/v1/pinterest')
+@crossdomain(origin='*')
 def api_pinterest():
     if 'url' in request.args:
         url = request.args['url']
@@ -42,6 +86,7 @@ def api_pinterest():
 
 
 @app.route('/api/v1/facebook')
+@crossdomain(origin='*')
 def api_facebook():
     if 'url' in request.args:
         url = request.args['url']
@@ -66,7 +111,7 @@ def api_facebook():
 
 #### Functions ####
 def get_time():
-    timestamp = datetime.datetime.now(pytz.utc).strftime('%Y-%m-%d %H:%M:%S')
+    timestamp = datetime.now(pytz.utc).strftime('%Y-%m-%d %H:%M:%S')
     return timestamp
 
 
